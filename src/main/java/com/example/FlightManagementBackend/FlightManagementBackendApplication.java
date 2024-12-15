f9d776d8cac14daa84f5568b07bf3511bdea079d
Maryam <maryam.shaheen@keyin.com>
2024-12-15 12:54:30 -0330

    Add .gitignore and initial backend files

---
+++ src/main/java/com/example/FlightManagementBackend/FlightManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementBackendApplication.class, args);
+	}
+
+}
